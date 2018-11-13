132dba2d249549574f22e30f2da7685886618fea
Itamar Shenhar <itamar8910@gmail.com>
2018-11-13 23:20:04 +0200

    basic os module

---
+++ os/os.py
@@ -0,0 +1,24 @@
+
+import cpu.cpu as cpu
+
+PROGRAM_INIT_ADDR = 400  # TODO: change this to allow multiple programs
+
+def load_program(instructions, init_addr):
+    for instr_i, instr in enumerate(instructions):
+        cpu.mem_set(instr, init_addr + instr_i)
+
+def reset_cpu_state():
+    cpu.MEM = {}
+    for reg in cpu.REGS:
+        cpu.REGS[reg] = 0
+
+def run_program(instructions):
+    load_program(instructions, PROGRAM_INIT_ADDR)
+    cpu.reg_set('IP', PROGRAM_INIT_ADDR)
+    cpu.start()  # run until HALT instruction
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
