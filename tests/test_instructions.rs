b03de9bc5d18533f178baae333daed2583aab4b1
Itamar Shenhar <itamar8910@gmail.com>
2018-11-26 22:17:06 +0200

    complete instructions tests

--- tests/test_instructions.rs
+++ tests/test_instructions.rs
@@ -272,4 +272,347 @@ fn test_tste_imm_yes(){
 }
 
 
-// TODO: test rest of instructions
+// tests bellow were converted from py test with regex
+
+#[test]
+fn test_tstg_yes(){
+    let program = "
+        MOV R1 4
+        MOV R2 3
+        TSTG R1 R2 
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::ZR), 1);
+}
+
+
+
+
+#[test]
+fn test_tstg_no(){
+    let program = "
+        MOV R1 3
+        MOV R2 4
+        TSTG R1 R2 
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::ZR), 0);
+}
+
+#[test]
+fn test_tstl_yes(){
+    let program = "
+        MOV R1 2
+        MOV R2 3
+        TSTL R1 R2 
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::ZR), 1);
+}
+
+#[test]
+fn test_tstl_no(){
+    let program = "
+        MOV R1 3
+        MOV R2 2
+        TSTL R1 R2 
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::ZR), 0);
+}
+
+// #[test]
+// fn test_rel_address(){
+//     let program = "
+//         MOV R1 3
+//         JUMP SKIP
+//         MOV R1 4
+//         SKIP:
+//         HALT
+//     ";
+// 	let mut os = OS::new();
+// 	let res = os.assemble_and_run(program);
+// }
+
+#[test]
+fn test_jump(){
+    let program = "
+        MOV R1 3
+        JUMP SKIP
+        MOV R1 4
+        SKIP:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 3);
+}
+#[test]
+fn test_jump2(){
+    let program = "
+        MOV R1 3
+        JUMP SKIP
+        L2:
+        MOV R1 4
+        JUMP END
+        SKIP:
+        JUMP L2
+        MOV R1 5
+        END:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 4);
+}
+
+#[test]
+fn test_tjump_pos(){
+    let program = "
+        MOV R1 3
+        TSTE R1 3
+        TJMP SKIP
+        MOV R1 4
+        SKIP:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 3);
+}
+
+#[test]
+fn test_tjump_neg(){
+    let program = "
+        MOV R1 3
+        TSTE R1 2
+        TJMP SKIP
+        MOV R1 4
+        SKIP:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 4);
+}
+
+#[test]
+fn test_fjump_pos(){
+    let program = "
+        MOV R1 3
+        TSTE R1 2
+        FJMP SKIP
+        MOV R1 4
+        SKIP:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 3);
+}
+
+#[test]
+fn test_fjump_neg(){
+    let program = "
+        MOV R1 3
+        TSTE R1 3
+        FJMP SKIP
+        MOV R1 4
+        SKIP:
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 4);
+}
+
+#[test]
+fn test_push_imm(){
+    let program = "
+        MOV R1 1
+        PUSH R1 
+        MOV R2 2
+        PUSH R2
+        POP R1
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 2);
+}
+
+#[test]
+fn test_push(){
+    let program = "
+        MOV R1 1
+        MOV R2 3
+        PUSH R1 
+        PUSH R2
+        POP R1
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 3);
+}
+
+#[test]
+fn test_pop(){
+    let program = "
+        MOV R2 1
+        MOV R1 4
+        PUSH R2 
+        PUSH R1
+        POP R2
+        POP R1 
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R2), 4);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 1);
+}
+
+#[test]
+fn test_call_ret_simple(){
+    let program = "
+        JUMP MAIN
+        FOO:
+        MOV R2 2
+        RET
+        MAIN:
+        MOV R1  1
+        CALL FOO
+        MOV R3 3
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 1);
+	assert_eq!(os.cpu.regs.get(&Register::R2), 2);
+	assert_eq!(os.cpu.regs.get(&Register::R3), 3);
+}
+
+#[test]
+fn test_call_ret_with_args_and_retval(){
+    let program = "
+        JUMP MAIN
+        ADD:
+        ADD R1 BP 3
+        LOAD R1 R1
+        ADD R2 BP 4
+        LOAD R2 R2
+        ADD R1 R1 R2
+        ADD R2 BP 2
+        STR R2 R1
+        RET
+        MAIN:
+        MOV R3 1
+        PUSH R3
+        MOV R3 2
+        PUSH R3
+        MOV R3 0
+        PUSH R3
+        CALL ADD
+        POP R1
+        HALT
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 3);
+}
+
+#[test]
+fn test_call_multiple(){
+    let program = "
+    JUMP MAIN
+    MAIN:
+    CALL FOO1
+    MOV R1 5
+    HALT
+    FOO2:
+    CALL FOO3
+    RET
+    FOO1:
+    CALL FOO2
+    RET
+    FOO3:
+    RET
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 5);
+}
+
+#[test]
+fn test_recursion_fiboncci(){
+    let program = "
+    JUMP MAIN
+    MAIN:
+    MOV R3 6
+    PUSH R3 
+    MOV R3 0
+    PUSH R3
+    CALL FIBBO
+    POP R1
+    HALT
+    FIBBO:
+    PUSH R1
+    PUSH R2
+    PUSH R4
+    ADD R4 BP 3
+    LOAD R4 R4
+    TSTG R4 1
+    TJMP RECURSE
+    ADD R1 BP 2
+    STR R1 R4
+    JUMP FIBO_RET
+    RECURSE:
+    ADD R4 R4 -1
+    PUSH R4 
+    MOV R3 0
+    PUSH R3
+    CALL FIBBO
+    POP R1
+    POP R4
+    ADD R4 R4 -1
+    PUSH R4
+    MOV R3 0
+    PUSH R3
+    CALL FIBBO
+    POP R2
+    POP R4
+    ADD R1 R1 R2
+    ADD R2 BP 2
+    STR R2 R1
+    FIBO_RET:
+    POP R4
+    POP R2
+    POP R1
+    RET
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(os.cpu.regs.get(&Register::R1), 8);
+}
+
+#[test]
+fn test_program_ret_val(){
+    let program = "
+    MAIN:
+    ADD R1 BP 2
+    STR R1 3
+    RET
+    ";
+	let mut os = OS::new();
+	let res = os.assemble_and_run(program);
+	assert_eq!(res, 3);
+}
