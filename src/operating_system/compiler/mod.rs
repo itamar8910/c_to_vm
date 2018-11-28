ff2c277c6e49a23a23b17421cf3140d472ef887e
Itamar Shenhar <itamar8910@gmail.com>
2018-11-28 17:25:20 +0200

    registering a function

--- src/operating_system/compiler/mod.rs
+++ src/operating_system/compiler/mod.rs
@@ -2,11 +2,19 @@ extern crate serde_json;
 
 use std::process::Command;
 use std::collections::HashMap;
+use ::cpu::instructions::{Register};
 
 const PATH_TO_PY_EXEC : &str = "src/operating_system/compiler/parser/venv/bin/python";
 const PATH_TO_PARSER : &str = "src/operating_system/compiler/parser/to_ast_json.py";
 
-pub fn get_ast_json(path_to_c_source: &str) -> serde_json::Value{
+// typedef ast Node = JSON value
+use self::serde_json::Value as Node;
+fn node_as_string(node: &Node) -> String{
+    // helper function because we only has node.as_str()
+    String::from(node.as_str().unwrap())
+}
+
+pub fn get_ast_json(path_to_c_source: &str) -> Node{
     let output = Command::new(PATH_TO_PY_EXEC)
                         .arg(PATH_TO_PARSER)
                         .arg(path_to_c_source)
@@ -18,12 +26,31 @@ pub fn get_ast_json(path_to_c_source: &str) -> serde_json::Value{
     serde_json::from_str(&json_str).expect("parser output is not JSON serializable")
 }
 
+struct VariableData{
+    name: String,
+    varType: String,
+    offset: u32,
+}
+
+struct FuncData{
+    scopeData: ScopeData,
+    regsUsed: Vec<Register>,
+    returnType: String,
+}
 struct ScopeData{
     name : String,
+    parentScope: String,
+    variables: HashMap<String, VariableData>,
+}
+
+// a scope-like object, i.e either a function or a regular scope
+enum ScopeLike{
+    Func(FuncData),
+    Scope(ScopeData),
 }
 
 pub struct Compiler{
-    scope_to_data  : HashMap<String, ScopeData>,
+    scope_to_data  : HashMap<String, ScopeLike>,
     tmp_label_count: u32,
 }
 
@@ -33,9 +60,55 @@ impl Compiler{
         Compiler {scope_to_data : HashMap::new(), tmp_label_count: 0}
     }
 
+    fn node_type(node: &Node) -> String{
+        node_as_string(&node["type"])
+    }
 
-    fn code_gen(&mut self, node : &serde_json::Value, scope : String, code: &mut Vec<String>){
+    // generates code, inserts generated code into the 'code' parameter
+    // we want to get code as a paramter rather that having it as a member of Compiler,
+    // so we can post-process the code generated for a specific object.
+    // an example for usefulness of this is knowing which registers we need to save in a function.
+    fn code_gen(&mut self, node : &Node, scope : &String, code: &mut Vec<String>){
+        
+    }
 
+    fn get_var_type_and_size(&self, node: &Node) -> (String, u32){
+        (String::from("int"), 1) // TODO generalize
+    }
+
+    fn regiser_function(&mut self, node : &Node, parentScope: &String){
+
+        // collect variables
+        let mut next_var_offset = 0;
+        let mut variables = HashMap::new();
+        let block_items = node["block_items"].as_array().unwrap();
+        for item in block_items.iter(){
+            if Compiler::node_type(item) == "Decl"{
+                let var_name = node_as_string(&item["name"]);
+                let (var_type, var_size) = self.get_var_type_and_size(node);
+                variables.insert(var_name.clone(), VariableData{
+                    name: var_name.clone(),
+                    varType: var_type,
+                    offset: next_var_offset,
+                });
+                next_var_offset += var_size;
+            }
+        }
+
+        let funcName = node_as_string(&node["decl"]["name"]);
+        let regsUsed = Vec::new();
+        let funcRetType = String::from("int");
+        let scopeData = ScopeData{
+                name: funcName.clone(),
+                parentScope: parentScope.clone(),
+                variables: variables,
+            };
+        self.scope_to_data.insert(funcName, ScopeLike::Func(
+            FuncData{
+                scopeData: scopeData,
+                regsUsed: regsUsed,
+                returnType: funcRetType,
+        }));
     }
 
     fn _compile(&mut self, path_to_c_source : &str) -> Vec<String>{
@@ -45,9 +118,10 @@ impl Compiler{
         // assuming only main func
         let main_func = &ast["ext"][0];
         let main_decl = &main_func["decl"];
-        assert_eq!(main_decl["name"], serde_json::Value::String("main".to_string()));
-        assert_eq!(main_decl["type"]["type"]["type"]["names"][0], serde_json::Value::String("int".to_string()));
-        self.code_gen(&main_func, "".to_string(), &mut code);
+        assert_eq!(main_decl["name"], Node::String("main".to_string()));
+        assert_eq!(main_decl["type"]["type"]["type"]["names"][0], Node::String("int".to_string()));
+
+        self.code_gen(&main_func, &"".to_string(), &mut code);
 
         code
     }   
