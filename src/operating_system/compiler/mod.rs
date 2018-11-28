f10573ae9f83d361cb3c2551bde71b9abb8d29bd
Itamar Shenhar <itamar8910@gmail.com>
2018-11-28 14:25:21 +0200

    compiler struct

--- src/operating_system/compiler/mod.rs
+++ src/operating_system/compiler/mod.rs
@@ -1,6 +1,7 @@
 extern crate serde_json;
 
 use std::process::Command;
+use std::collections::HashMap;
 
 const PATH_TO_PY_EXEC : &str = "src/operating_system/compiler/parser/venv/bin/python";
 const PATH_TO_PARSER : &str = "src/operating_system/compiler/parser/to_ast_json.py";
@@ -16,6 +17,45 @@ pub fn get_ast_json(path_to_c_source: &str) -> serde_json::Value{
 
     serde_json::from_str(&json_str).expect("parser output is not JSON serializable")
 }
-// fn compile(text: &str) -> Vec<String>{
-//     let code: Vec<String> = Vec::new();
-// }
+
+struct ScopeData{
+    name : String,
+}
+
+pub struct Compiler{
+    scope_to_data  : HashMap<String, ScopeData>,
+    tmp_label_count: u32,
+}
+
+impl Compiler{
+
+    pub fn new() -> Compiler{
+        Compiler {scope_to_data : HashMap::new(), tmp_label_count: 0}
+    }
+
+
+    fn code_gen(&mut self, node : &serde_json::Value, scope : String, code: &mut Vec<String>){
+
+    }
+
+    fn _compile(&mut self, path_to_c_source : &str) -> Vec<String>{
+        let mut code: Vec<String> = Vec::new();
+        let ast = get_ast_json(path_to_c_source);
+        
+        // assuming only main func
+        let main_func = &ast["ext"][0];
+        let main_decl = &main_func["decl"];
+        assert_eq!(main_decl["name"], serde_json::Value::String("main".to_string()));
+        assert_eq!(main_decl["type"]["type"]["type"]["names"][0], serde_json::Value::String("int".to_string()));
+        self.code_gen(&main_func, "".to_string(), &mut code);
+
+        code
+    }   
+
+
+    pub fn compile(path_to_c_source : &str) -> Vec<String>{
+        let mut instance = Compiler::new();
+        instance._compile(path_to_c_source)
+    }
+
+}
